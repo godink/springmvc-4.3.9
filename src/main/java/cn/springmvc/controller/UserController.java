eeee62cde01d815b02f0734d7ee87fae92294544
hong.chen <657442592@qq.com>
2017-10-05 16:10:51 +0800

    springmvc4.3.9,  可以运行本地

--- src/main/java/cn/springmvc/controller/UserController.java
+++ src/main/java/cn/springmvc/controller/UserController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import cn.springmvc.model.User;
@@ -17,7 +18,7 @@ public class UserController {
 	@Autowired
 	private IUserService userService;
 	
-	@RequestMapping("/getUser")
+	@GetMapping("/getUser")
 	public String getUser(HttpServletRequest request, Model model) {
 		String userId = request.getParameter("userId");
 		User user = userService.getUserById(Integer.parseInt(userId));
